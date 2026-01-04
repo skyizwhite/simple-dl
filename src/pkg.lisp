e138e375b61436803371b7e8a6f0ef52720928f1
Akira Tempaku <paku@skyizwhite.dev>
2026-01-04 21:48:48 +0900

    step 2

--- src/pkg.lisp
+++ src/pkg.lisp
@@ -10,3 +10,21 @@
 
 (defun d-variable (data)
   (make-instance 'd-variable :data data))
+
+(defclass d-function () ())
+
+(defmethod call ((f d-function) (input d-variable))
+  (d-variable (forward f (d-variable-data input))))
+
+(defmethod forward ((f d-function) x)
+  (error "Not Implemented Error"))
+
+(defmacro def-d-fun (name &key forward)
+  `(progn
+     (defclass ,name (d-function) ())
+     (defun ,name () (make-instance ',name))
+     (defmethod forward ((f ,name) ,@(first forward))
+       ,@(rest forward))))
+
+(def-d-fun d-square
+  :forward ((x) (* x x)))
