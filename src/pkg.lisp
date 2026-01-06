84ec6524aa67fb39b4c065d9caee732e9b8587f0
Akira Tempaku <paku@skyizwhite.dev>
2026-01-06 14:06:17 +0900

    step 8

--- src/pkg.lisp
+++ src/pkg.lisp
@@ -22,11 +22,14 @@
 
 (defmethod backward ((v d-variable) &optional gy)
   (declare (ignore gy))
-  (let ((f (d-variable-creator v)))
-    (when f
-      (let ((x (d-function-input f)))
-        (setf (d-variable-grad x) (backward f (d-variable-grad v)))
-        (backward x)))))
+  (let ((funcs (list (d-variable-creator v))))
+    (loop :while funcs
+          :for f := (pop funcs)
+          :for x := (d-function-input f)
+          :for y := (d-function-output f)
+          :do (setf (d-variable-grad x) (backward f (d-variable-grad y)))
+              (when (d-variable-creator x)
+                (push (d-variable-creator x) funcs)))))
 
 (defclass d-function () 
   ((input  :accessor d-function-input
@@ -69,30 +72,3 @@
             `(call ,step ,form))
           (reverse steps)
           :initial-value x))
-
-(d-variable-data (call-> (d-variable 0.5)
-                         (d-square)
-                         (d-exp)
-                         (d-square)))
-
-(defun numerical-diff (f x &optional (eps 1e-4))
-  (let* ((x0 (d-variable (- (d-variable-data x)
-                            eps)))
-         (x1 (d-variable (+ (d-variable-data x)
-                            eps)))
-         (y0 (call f x0))
-         (y1 (call f x1)))
-    (/ (- (d-variable-data y1) (d-variable-data y0))
-       (* 2 eps))))
-
-(numerical-diff (d-square) (d-variable 2.0))
-
-;(def-d-fun d-f
-;  :forward ((x) (d-variable-data (call-> (d-variable x)
-;                                         (d-square)
-;                                         (d-exp)
-;                                         (d-square)))))
-
-;(let ((x (d-variable 0.5)))
-;  (numerical-diff (d-f) x))
-
